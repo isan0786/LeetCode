a614357c09c6cf04dff51f6a5aae401e15687cbb
Ishan Anand <ishantechandothers@gmail.com>
2023-07-31 09:51:12 -0400

    Update contains-duplicate.py
    added complexities

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -6,3 +6,6 @@ class Solution:
                 return True
             hashmap[n] = 1
         return False
+
+# space complexity of the dictionary is O(n) because each n elements is stored in the memory.
+# time complexity of the dictionary is also O(n), the number of elements that we are iterating through are n
