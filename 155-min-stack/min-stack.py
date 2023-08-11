4c3635d4a56945cf2dc920847a2b812f6c1eeb08
Ishan Anand <ishantechandothers@gmail.com>
2023-08-11 06:47:34 -0400

    Time: 60 ms (90.34%) | Memory: 20.5 MB (26.43%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -7,6 +7,7 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        # maintaining the minimum value at all the time in an another stack
         val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
 
@@ -23,7 +24,7 @@ class MinStack:
         return self.minStack[-1]
         
 
-
+# to get the mininum value from the stack takes O(1) time complexity, space complexity is O(n)  because we are storing the elements in a list
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
