0746c3cca7a8889e27035c9f2ac1915a2e359f3f
Ishan Anand <ishantechandothers@gmail.com>
2024-02-29 04:14:52 -0500

    Time: 42 ms (49.10%) | Memory: 17.5 MB (89.59%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,26 +7,37 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        # # Solution with recursion
+        # # Solution with recursion -------- 
         # if not root:
         #     return 0
         # # 1 is for the root
         # return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
 
-        # iterative BFS 
-        if not root:
-            return 0
+        # # iterative BFS ---------------- 
+        # if not root:
+        #     return 0
         
-        level = 0
-        # this deque function is working as a queue
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node=q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
+        # level = 0
+        # # this deque function is working as a queue
+        # q = deque([root])
+        # while q:
+        #     for i in range(len(q)):
+        #         node=q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level += 1
+        # return level
 
+        # iterative DFS - pre-order
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left,depth+1])
+                stack.append([node.right,depth+1])
+        
+        return res
