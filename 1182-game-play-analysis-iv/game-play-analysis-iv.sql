2b1573eaacb2f0e79f5f3e9d5d571e48c5291cd5
Ishan Anand <ishantechandothers@gmail.com>
2024-03-03 06:19:46 -0500

    Time: 1021 ms (56.86%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 SELECT
   ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM
